8c3228c8944be60c684928d910fc0d9db6162900
Javed Bagwan <javed460@gmail.com>
2024-01-07 09:00:35 +0530

    Added data jpa and h2 dependency

--- domain/src/main/java/org/example/domain/Person.java
+++ domain/src/main/java/org/example/domain/Person.java
@@ -1,4 +1,20 @@
 package org.example.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
 }
