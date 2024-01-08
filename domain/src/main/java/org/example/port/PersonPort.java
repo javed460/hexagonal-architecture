cc3ba47d9d3cbf1042795b1f7c1f81483dcbd49e
Javed Bagwan <javed460@gmail.com>
2024-01-08 09:40:36 +0530

    Added Person Port

--- domain/src/main/java/org/example/port/PersonPort.java
+++ domain/src/main/java/org/example/port/PersonPort.java
@@ -1,5 +1,13 @@
 package org.example.port;
 
+import org.example.domain.Person;
+
 public interface PersonPort {
+
+    Long createPerson(Person person);
+
+    Person getPerson(String id);
+
+    Person updatePerson(Person person);
 
 }
