64cadab8fe90e4e16dbff3e1d700b06618ee9c46
Javed Bagwan <javed460@gmail.com>
2024-01-10 07:54:14 +0530

    updated portAdapter

--- integration/src/main/java/org/example/portadapter/PersonPortAdapter.java
+++ integration/src/main/java/org/example/portadapter/PersonPortAdapter.java
@@ -2,25 +2,31 @@ package org.example.portadapter;
 
 import lombok.RequiredArgsConstructor;
 import org.example.domain.Person;
+import org.example.exception.PersonNotFoundException;
 import org.example.port.PersonPort;
+import org.example.repo.PersonRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PersonPortAdapter implements PersonPort {
 
+    private final PersonRepository personRepository;
+
     @Override
     public Long createPerson(Person person) {
-        return null;
+        Person savedPerson = personRepository.save(person);
+        return savedPerson.getId();
     }
 
     @Override
     public Person getPerson(String id) {
-        return null;
+        return personRepository.findById(Long.parseLong(id))
+                .orElseThrow(() -> new PersonNotFoundException(Long.parseLong(id)));
     }
 
     @Override
     public Person updatePerson(Person person) {
-        return null;
+        return personRepository.save(person);
     }
 }
