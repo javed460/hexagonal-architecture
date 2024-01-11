18a053f18839da673e4c04974132bc0a7b653197
Javed Bagwan <javed460@gmail.com>
2024-01-11 08:52:27 +0530

    Added DTOs

---
+++ integration/src/main/java/org/example/dto/AddressDTO.java
@@ -0,0 +1,11 @@
+package org.example.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressDTO {
+    private String line1;
+    private String line2;
+    private String city;
+    private String pincode;
+}
