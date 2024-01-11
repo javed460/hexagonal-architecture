487ea0e70937e237005efa026413aa79632093e0
Javed Bagwan <javed460@gmail.com>
2024-01-11 08:53:06 +0530

    Added GlobalExceptionHandler

---
+++ integration/src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(PersonNotFoundException.class)
+    public ResponseEntity<Object> handlePersonNotFoundException(PersonNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
